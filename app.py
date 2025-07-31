c1ee4ebd28a7383e7e01af51a92b7e848642d2b0
Tylon <tjhunt0707@gmail.com>
2025-07-31 14:37:50 -0700

    Put the first two images

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+#starter code/Boiler plate
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
     
     return render_template("index.html")
-
+#connection setup for game page 
+@app.route("/Gamepg")
+def gamePg():
+    return render_template("Gamepg.html")
 # main driver function
 if __name__ == '__main__':
 
