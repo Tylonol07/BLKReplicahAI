200fe0c4fe254e2fa1a9c47c4aa022937613159b
Tylon <tjhunt0707@gmail.com>
2025-08-04 14:36:05 -0700

    All images are in. also made new routes

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    
     return render_template("index.html")
+
+@app.route("Quiz1")
+def quiz1():
+    return render_template("Quizpg1.html")
+
+@app.route("Quiz2")
+def quiz2():
+    return render_template("Quizpg2.html")
+
+@app.route("Ans1")
+def Ans1():
+    return render_template("ans1.html")
+
 #connection setup for game page 
 @app.route("/Gamepg")
 def gamePg():
